6e2e5ea045d781775bc8a85c334d40906cedb571
SimonN <65023744+s994n@users.noreply.github.com>
2024-01-16 13:32:41 +0000

    feat: Add and generate metadata

--- app/current-weather/city/[city]/page.tsx
+++ app/current-weather/city/[city]/page.tsx
@@ -7,13 +7,22 @@ import { WeatherInfoItems } from "@/components/organisms";
 // Services
 import { getCityCurrentWeather } from "@/services/api";
 
+export async function generateMetadata({ params }: { params: { city: string } }) {
+  const city = params.city;
+  return {
+    title: `${city} current weather`,
+    description: `The current weather for ${city}`,
+  };
+}
+
 const CityCurrentWeather = async ({ params }: { params: { city: string } }) => {
-  const currentWeather = await getCityCurrentWeather(params.city);
+  const city = params.city;
+  const currentWeather = await getCityCurrentWeather(city);
 
   return (
     <main>
       <StyledHeader variant="hero" caps>
-        Current Weather for {params.city}
+        Current Weather for {city}
       </StyledHeader>
       <WeatherInfoItems weatherData={currentWeather} />
     </main>
