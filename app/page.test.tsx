844862d88e34cd152629afab3a5f1040d2d601e7
SimonN <65023744+s994n@users.noreply.github.com>
2024-01-17 08:40:35 +0000

    fix: Update snapshot test to pass

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -1,34 +1,22 @@
-import React, { ReactNode } from "react";
-import { render } from "@testing-library/react";
-import Home from "./page";
-
-jest.mock("react-dom");
-
-// Mocking the SingleInputForm component to avoid warnings from
-// specific props of children that are not recognised as HTML attributes during the snapshot test
-jest.mock("@components/molecules/SingleInputForm", () => ({
-  __esModule: true,
-  default: ({
-    children,
-    action,
-    inputFieldType,
-    inputFieldVariant,
-    buttonVariant,
-    buttonAriaLabel,
-    buttonLabel,
-    ...props
-  }: {
-    children: ReactNode;
-    action: (formData: FormData) => Promise<void>;
-    inputFieldType: string;
-    inputFieldVariant: string;
-    buttonVariant: string;
-    buttonAriaLabel: string;
-    buttonLabel: string;
-  }) => <form {...props}>{children}</form>,
+jest.mock("next/navigation", () => ({
+  useRouter() {
+    return {
+      route: "/",
+      pathname: "",
+      query: "",
+      asPath: "",
+      push: jest.fn(),
+    };
+  },
 }));
 
-it("renders and matches snapshot", () => {
-  const { asFragment } = render(<Home />);
-  expect(asFragment()).toMatchSnapshot();
+import React from "react";
+import { render } from "@testing-library/react";
+import Home from "./page"; // Adjust the import path as needed
+
+describe("Home Page Snapshot", () => {
+  it("should match the snapshot", () => {
+    const { asFragment } = render(<Home />);
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
